0b04d80425dfb9756bc9f98f9cb3a9d35162b511
georgiaclemencon <georgia.clemencon@isen.yncrea.fr>
2024-04-28 21:32:56 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MasterProjet1"
+include(":app")
+ 

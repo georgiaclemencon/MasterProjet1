45e15129beb648ea2834f97705378b160a63887f
jade <jademouillot@gmail.com>
2024-04-29 16:10:28 +0200

    commit autres fichiers

--- app/src/main/java/com/example/masterprojet1/ContactForm.kt
+++ app/src/main/java/com/example/masterprojet1/ContactForm.kt
@@ -8,6 +8,20 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class ContactForm : ComponentActivity() {
     @SuppressLint("IntentReset")
@@ -41,5 +55,6 @@ class ContactForm : ComponentActivity() {
 
             }
         }
+
     }
 }
